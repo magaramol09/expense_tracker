6d3ac993bbc259435311ba7f310981641dd266fc
Amol Magar <magaramol09@gmail.com>
2023-01-25 23:00:59 -0800

    intial setup with next-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Button } from '@nextui-org/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -15,20 +16,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + NextUI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button>Next UI Button</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
